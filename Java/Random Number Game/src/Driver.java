34c3319d3996f6aa9d69ad8e164d49865978a25b
Hailfire <ashamshad96@gmail.com>
2013-07-15 17:23:04 -0500

    Addd another old project - random number generator

--- Java/Random Number Game/src/Driver.java
+++ Java/Random Number Game/src/Driver.java
@@ -1,25 +1,33 @@
-import java.util.Random;
+import java.util.*;
 
-public class Driver 
+public class Driver
+
 {
 
-	public static void main(String[] args) 
-	{
-		Random rand = new Random();
-		
-		int number = rand.nextInt(51); //number the user is trying to guess
-		int guess = -1;				   //number that user has guessed
-		boolean user=false;  
-		String answer = "yes";
-		String yes =  "yes";
-		while(user == false )
-		{
-			System.out.println("is the number even?");
-			if(answer == yes)
-			{
-				
-			}
-		}
-	}
 
+	public static void main (String [] args)
+	{
+		int lvl = 8;
+		System.out.println("you are lvl "+lvl);
+		
+		System.out.println("You see two paths, path 1 and path 2 which do you take, one leads to a castle the other leads to a small area");
+		
+		Scanner user = new Scanner (System.in);
+		String path1 = "1";
+		String path2 = "2";
+		String userchoice = user.nextLine();
+		if(userchoice.equals(path1))
+		{
+			System.out.println("an orc appears do you \n1 fight or \n2 run or \n3 surrender, \nthe orc is level 10 and deals 20 base damage plus 15 fire damge with no resistance while you are lvl 8 and do 30 base damage no elemental damage and resistance, your ring has a level 2 shock spell" );
+			++lvl;
+			System.out.println(lvl);	
+		}
+	
+	}
+	
 }
+	
+	
+	
+	
+
